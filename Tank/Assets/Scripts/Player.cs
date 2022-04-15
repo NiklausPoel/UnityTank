2eb3c0f63120777a94052a63087d5fc4ac10ce32
Proeld <1464035086.com>
2022-04-15 22:31:55 +0800

    init

---
+++ Tank/Assets/Scripts/Player.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 3;
+
+    //用来控制坦克左右移动
+    private SpriteRenderer sr;
+    public Sprite[] tankSprite;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //水平方向 左右键
+        float h = Input.GetAxisRaw("Horizontal");
+        //水平方向移动
+        transform.Translate(Vector3.right * h * moveSpeed * Time.deltaTime, Space.World);
+
+        //left
+        if(h < 0)
+        {
+            sr.sprite = tankSprite[3];
+        }
+        //right
+        else if(h > 0)
+        {
+            sr.sprite = tankSprite[1];
+        }
+
+        //垂直方向 上下键
+        float v = Input.GetAxisRaw("Vertical");
+        //垂直方向移动
+        transform.Translate(Vector3.up * v * moveSpeed * Time.deltaTime, Space.World);
+
+        //up
+        if(v < 0)
+        {
+            sr.sprite = tankSprite[2];
+        }
+        //down
+        else if(v > 0)
+        {
+            sr.sprite = tankSprite[0];
+        }
+
+    }
+}
---
+++ Unity.md
@@ -0,0 +1,18 @@
+FIXME:Unity
+球吃方块（完成）：https://www.bilibili.com/video/BV18U4y187ug
+ 
+坦克大战：https://www.bilibili.com/video/BV1PW41197Su?p=9&spm_id_from=pageDriver
+
+FIXME：创建森林场景
+https://www.bilibili.com/video/BV1bs411A7i8?from=search&seid=12739963341804134544&spm_id_from=333.337.0.0
+
+Unity账号：15311271552 同号微信
+
+VSCode编译C、C#
+dotnet new console
+dotnet restore
+dotnet run
+ 
+Unity5中文手册：https://www.bookstack.cn/read/Unity55/README.md
+
+
